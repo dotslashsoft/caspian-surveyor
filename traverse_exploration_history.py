599954b324c3dcc2d4332f1feeda11bd6bbd07dd
dotslashsoft <325615479+dotslashsoft@users.noreply.github.com>
2026-08-23 08:47:53 -0400

    Version 0.3.02
    - refactored traverse_exploration_history.py
        - placed recursion and traversal within
            traverse_exploration_jsonl()
        - traverse_exploration_jsonl() requires the
            system_dict argument
    
        - placed loading/parsing of last line in
            exploration_history.jsonl within
            load_latest_system_record()
        - load_latest_system_record() returns
            last_line
        - last_line is used for the system_dict
            argument
    
        - implemented 'if __name__ == "__main__":'
            to prevent program from running when
            called from another source

--- traverse_exploration_history.py
+++ traverse_exploration_history.py
@@ -1,29 +1,35 @@
 import main
 import json
 
-system_data_information = main.EXPLORATION_HISTORY_FILE
 
-with open(system_data_information, "r") as open_json:
-    last_line = (list(open_json)[-1])
+def load_latest_system_record():
 
-system_dict = json.loads(last_line)
+    system_data_information = main.EXPLORATION_HISTORY_FILE
 
-for section_name, section_data in system_dict.items():
+    with open(system_data_information, "r") as open_json:
+        last_line = list(open_json)[-1]
 
-    if section_name == "system":
-        for field_name, field_value in section_data.items():
-            print(field_name, field_value)
+    return json.loads(last_line)
 
-    if section_name == "summary":
-        for field_name, field_value in section_data.items():
-            print(field_name, field_value)
+def traverse_exploration_jsonl(system_dict):
+    for section_name, section_data in system_dict.items():
+        if section_name == "system":
+            for field_name, field_value in section_data.items():
+                print(field_name, field_value)
 
-    if section_name == "bodies":
-        for body_id, body_data in section_data.items():
-            for field_name, field_value in body_data.items():
+        if section_name == "summary":
+            for field_name, field_value in section_data.items():
+                print(field_name, field_value)
 
-                if field_name == "materials" and field_value is not None:
-                    for material in field_value:
-                        print(material["Name"], material["Percent"])
-                else:
-                    print(field_name, field_value)
+        if section_name == "bodies":
+            for body_id, body_data in section_data.items():
+                for field_name, field_value in body_data.items():
+                    if field_name == "materials" and field_value is not None:
+                        for material in field_value:
+                            print(material["Name"], material["Percent"])
+                    else:
+                        print(field_name, field_value)
+
+if __name__ == "__main__":
+    system_dict = load_latest_system_record()
+    traverse_exploration_jsonl(system_dict)
