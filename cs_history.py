1fd7631e1335070321d240c7b1e8fcea341f5aa1
dotslashsoft <325615479+dotslashsoft@users.noreply.github.com>
2026-09-02 11:09:02 -0400

    Version 0.8.5.0
    tl;dr intro to Logapalooza
    
    - Modified cs_runtime
        - Wrapped write_current_system_record() logic
            in try/except block.
        - Notable exceptions:
            - OSError
                - Exception logged and re-raised.
        - Added various DEBUG logging lines
            - Helps assist with tracking down
                Schrödinger's Heisenbug.
    
    - Modified cs_history
        - Wrapped load_current_system_data_to_dict()
            logic in try/except block.
        - Notable exceptions:
            - OSError
                - Exception logged and re-raised.
            - json.JSONDecodeError
                - Exception logged and re-raised.
    
        - Wrapped append_system_record_to_history_file()
            logic in try/except block.
        - Notable exceptions:
            - OSError
                - Exception logged and re-raised.

--- cs_history.py
+++ cs_history.py
@@ -11,26 +11,44 @@ CURRENT_SYSTEM_FILE = (cs_baseline_config.run_directory / "runtime" / "current_s
 class DataOrchestrator:
 
     def __init__(self, current_system_file=CURRENT_SYSTEM_FILE, exploration_history_file=EXPLORATION_HISTORY_FILE):
+        logger.debug("Initializing current_system_file, exploration_history_file, and current_system_data.")
         self.current_system_file = current_system_file
         self.exploration_history_file = exploration_history_file
         self.current_system_data = None
 
 
     def load_current_system_data_to_dict(self):
-        with self.current_system_file.open("r", encoding="utf-8") as file:
-            self.current_system_data = json.load(file)
+        
+        try:
+            logger.info("Opening current_system_file...")
+            with self.current_system_file.open("r", encoding="utf-8") as file:
+                self.current_system_data = json.load(file)
 
-        return self.current_system_data
+            return self.current_system_data
+        
+        except OSError:
+            logger.exception("Unable to load current system data.")
+            raise
 
+        except json.JSONDecodeError:
+            logger.exception("Unable to decode current system data.")
+            raise
 
     def append_system_record_to_history_file(self):
-        logger.debug("Reached append_system_record_to_history_file.")
-        if self.current_system_data is None:
-            logger.warning("Unable to append system history: ""no current system data loaded.")
-            return False
 
-        logger.debug("Reached actual with block to write to history file.")
-        with self.exploration_history_file.open("a", encoding="utf-8") as file:
-            file.write(json.dumps(self.current_system_data) + "\n")
+        logger.debug("Reached append_system_record_to_history_file.")
+
+        if self.current_system_data is None:
+            logger.error("Unable to append system history: no current system data loaded.")
+            raise RuntimeError("No current system data loaded.")
+
+        try:
+            logger.debug("Reached actual *with* block to write to history file.")
+            with self.exploration_history_file.open("a", encoding="utf-8") as file:
+                file.write(json.dumps(self.current_system_data) + "\n")
+
+        except OSError:
+            logger.exception("Unable to append current system data to exploration history file.")
+            raise
 
         return True
--- runtime/cs_runtime.py
+++ runtime/cs_runtime.py
@@ -1,6 +1,8 @@
 import json
 import bootstrap.cs_baseline_config as cs_baseline_config
+import logging
 
+logger = logging.getLogger(__name__)
 
 RUNTIME_DIRECTORY = (cs_baseline_config.run_directory / "runtime")
 CURRENT_SYSTEM_FILE = (RUNTIME_DIRECTORY / "current_system.json")
@@ -11,11 +13,19 @@ def write_current_system_record(system_record: dict | None) -> bool:
     if system_record is None:
         return False
 
-    RUNTIME_DIRECTORY.mkdir(parents=True, exist_ok=True)
+    try:
+        logger.debug("Creating RUNTIME_DIRECTORY if it doesn't exist.")
+        RUNTIME_DIRECTORY.mkdir(parents=True, exist_ok=True)
 
-    with TEMP_SYSTEM_FILE.open("w", encoding="utf-8") as file:
-        json.dump(system_record, file)
+        logger.debug("Opening TEMP_SYSTEM_FILE and writing system record.")
+        with TEMP_SYSTEM_FILE.open("w", encoding="utf-8") as file:
+            json.dump(system_record, file)
 
-    TEMP_SYSTEM_FILE.replace(CURRENT_SYSTEM_FILE)
+        logger.debug("Attempting to overwrite CURRENT_SYSTEM_FILE with TEMP_SYSTEM_FILE.")
+        TEMP_SYSTEM_FILE.replace(CURRENT_SYSTEM_FILE)
+
+    except OSError:
+        logger.exception("Unable to write current system record.")
+        raise
 
     return True
